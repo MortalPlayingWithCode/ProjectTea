23ec8e5b969813ab51a7d5d0e86550fcf0e5a1f3
MortalPlayingWithCode <tranphongzhouzhou@gmail.com>
2024-11-12 00:30:27 +0700

    Add project files.

---
+++ ProjectTea/Data/HangHoaLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectTea.Data;
+
+public partial class HangHoaLog
+{
+    public int LogId { get; set; }
+
+    public int? MaHh { get; set; }
+
+    public string? Action { get; set; }
+
+    public DateTime? ChangeDate { get; set; }
+
+    public string? Details { get; set; }
+}
